24b9f939234ab49912814f041a5116aa8fe05e32
amethaaa <amethajayawardena@gmail.com>
2025-09-26 20:00:20 +0530

    add code to validate the length entered by user on cli

--- gen_password.py
+++ gen_password.py
@@ -1,6 +1,5 @@
 import secrets
 import string
-import sys
 import argparse
 
 def generate_password(length):
@@ -8,13 +7,25 @@ def generate_password(length):
     password = "".join(secrets.choice(alphabet) for _ in range(length))
     print(f"Password: {password}")
 
+def valid_length(length):
+    try:
+        ilength = int(length)
+
+        if ilength < 4 or ilength > 128:
+            raise argparse.ArgumentTypeError("The length of the password that can be generated is between 4 and 128")
+        else:
+            return ilength
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"Invalid value passed as length {length}")
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2 or len(sys.argv) > 2:
-        print("Usage: gen_password.py <length_of_password>")
-        sys.exit(1)
     
     parser = argparse.ArgumentParser(description="Generate a password for your convienience")
-    parser.add_argument("length", type=int, help="Length of password")
+
+    parser.add_argument("length", type=valid_length, help="Length of password")
+
+    args = parser.parse_args()
+    print(args.length)
 
     try:
         generate_password(parser.parse_args().length)
